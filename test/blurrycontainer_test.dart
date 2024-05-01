f917c9f8b7b992466e20d1df6f1d0d344df046b1
kishan-dhankecha <dhankechakishan@gmail.com>
2024-05-01 15:50:48 +0530

    [fix] tests updated

--- test/blurrycontainer_test.dart
+++ test/blurrycontainer_test.dart
@@ -3,97 +3,106 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('BlurryContainer renders child widget', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: Scaffold(
-          body: BlurryContainer(
-            child: Text('Hello, World!'),
+  group('BlurryContainer Widget Tests', () {
+    testWidgets('BlurryContainer renders child widget', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: BlurryContainer(
+              child: Text('Hello, World!'),
+            ),
           ),
         ),
-      ),
-    );
+      );
 
-    expect(find.text('Hello, World!'), findsOneWidget);
-  });
+      expect(find.descendant(of: find.byType(BlurryContainer), matching: find.text('Hello, World!')), findsOneWidget);
+    });
 
-  testWidgets('BlurryContainer default properties', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: Scaffold(
-          body: BlurryContainer(
-            child: Text('Hello, World!'),
+    group('BlurryContainer properties', () {
+      testWidgets('BlurryContainer default properties', (WidgetTester tester) async {
+        await tester.pumpWidget(
+          const MaterialApp(
+            home: Scaffold(
+              body: BlurryContainer(
+                child: SizedBox(),
+              ),
+            ),
           ),
-        ),
-      ),
-    );
+        );
 
-    final container = tester.widget<BlurryContainer>(find.byType(BlurryContainer));
+        final widget = tester.widget<BlurryContainer>(find.byType(BlurryContainer));
 
-    expect(container.blur, equals(5));
-    expect(container.elevation, equals(0));
-    expect(container.padding, equals(const EdgeInsets.all(8)));
-    expect(container.color, equals(Colors.transparent));
-    expect(container.borderRadius, equals(BorderRadius.circular(20)));
-  });
+        expect(widget.blur, equals(5));
+        expect(widget.elevation, equals(0));
+        expect(widget.padding, equals(const EdgeInsets.all(8)));
+        expect(widget.color, equals(Colors.transparent));
+        expect(widget.shadowColor, equals(Colors.black26));
+        expect(widget.borderRadius, equals(BorderRadius.circular(20)));
+      });
 
-  testWidgets('BlurryContainer custom properties', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      MaterialApp(
-        home: Scaffold(
-          body: BlurryContainer(
-            blur: 10,
-            elevation: 2,
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(10),
-            child: const Text('Hello, World!'),
+      testWidgets('BlurryContainer custom properties', (WidgetTester tester) async {
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: BlurryContainer(
+                blur: 10,
+                elevation: 2,
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                color: Colors.blue,
+                shadowColor: Colors.red,
+                borderRadius: BorderRadius.circular(10),
+                child: const SizedBox(),
+              ),
+            ),
           ),
-        ),
-      ),
-    );
+        );
 
-    final container = tester.widget<BlurryContainer>(find.byType(BlurryContainer));
+        final widget = tester.widget<BlurryContainer>(find.byType(BlurryContainer));
 
-    expect(container.blur, equals(10));
-    expect(container.elevation, equals(2));
-    expect(container.padding, equals(const EdgeInsets.symmetric(horizontal: 16, vertical: 8)));
-    expect(container.color, equals(Colors.blue));
-    expect(container.borderRadius, equals(BorderRadius.circular(10)));
-  });
+        expect(widget.blur, equals(10));
+        expect(widget.elevation, equals(2));
+        expect(widget.padding, equals(const EdgeInsets.symmetric(horizontal: 16, vertical: 8)));
+        expect(widget.color, equals(Colors.blue));
+        expect(widget.shadowColor, equals(Colors.red));
+        expect(widget.borderRadius, equals(BorderRadius.circular(10)));
+      });
+    });
 
-  testWidgets('BlurryContainer.square constructor', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: Scaffold(
-          body: BlurryContainer.square(
-            dimension: 100,
-            child: Text('Hello, World!'),
+    group('BlurryContainer constructor', () {
+      testWidgets('BlurryContainer.square constructor', (WidgetTester tester) async {
+        await tester.pumpWidget(
+          const MaterialApp(
+            home: Scaffold(
+              body: BlurryContainer.square(
+                dimension: 100,
+                child: SizedBox(),
+              ),
+            ),
           ),
-        ),
-      ),
-    );
+        );
 
-    final container = tester.widget<BlurryContainer>(find.byType(BlurryContainer));
+        final widget = tester.widget<BlurryContainer>(find.byType(BlurryContainer));
 
-    expect(container.height, equals(100));
-    expect(container.width, equals(100));
-  });
+        expect(widget.height, equals(100));
+        expect(widget.width, equals(100));
+      });
 
-  testWidgets('BlurryContainer.expand constructor', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: Scaffold(
-          body: BlurryContainer.expand(
-            child: Text('Hello, World!'),
+      testWidgets('BlurryContainer.expand) constructor', (WidgetTester tester) async {
+        await tester.pumpWidget(
+          const MaterialApp(
+            home: Scaffold(
+              body: BlurryContainer.expand(
+                child: SizedBox(),
+              ),
+            ),
           ),
-        ),
-      ),
-    );
+        );
 
-    final container = tester.widget<BlurryContainer>(find.byType(BlurryContainer));
+        final widget = tester.widget<BlurryContainer>(find.byType(BlurryContainer));
 
-    expect(container.height, equals(double.infinity));
-    expect(container.width, equals(double.infinity));
+        expect(widget.height, equals(double.infinity));
+        expect(widget.width, equals(double.infinity));
+      });
+    });
   });
 }
